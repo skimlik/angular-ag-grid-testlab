a271f18f0a76f583d00fa971474b527015f42ece
skimlik <kimlik.s@gmail.com>
2020-07-09 12:02:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ag-grid-testlab
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ag-grid-testlab)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+    columnDefs = [
+        {field: 'make' },
+        {field: 'model' },
+        {field: 'price'}
+    ];
+
+    rowData = [
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 }
+    ];
+}
